8fbedbdaa6391c3252387f119bc696e62012fb67
Sebi <sebastijan.bogdan@outlook.com>
2019-01-08 19:35:44 +0100

    4 Tests not working.

--- caravan.cpp
+++ caravan.cpp
@@ -15,7 +15,7 @@
 #include <stdlib.h>
 
 typedef struct _node{
-  PackAnimal pack_animal;
+  PackAnimal animal;
   struct _node* next;
 }Node;
 
@@ -56,36 +56,71 @@ void delete_caravan(Caravan caravan)
 void add_pack_animal(Caravan caravan, PackAnimal animal)
 {
   Node* new_node = (Node*)malloc(sizeof(Node));
-  new_node->pack_animal = animal;
+  new_node->animal = animal;
   new_node->next = caravan->head;
   caravan->head = new_node;
 }
 
 void remove_pack_animal(Caravan caravan, PackAnimal animal)
 {
-  
+  if(caravan->head==0||animal==0)
+  return;
+  if(caravan->head->animal == animal)
+  {
+    Node* toBeDeleted=caravan->head;
+    caravan->head=caravan->head->next;
+    sfree(toBeDeleted);
+  }
+  else{
+    Node* currNode=caravan->head;
+    while(currNode->next != 0 && currNode->next->animal != animal){
+      currNode = currNode->next;
+    }
+
+  if(currNode->next != 0){
+    Node* toBeDeleted=currNode->next;
+    currNode->next=currNode->next->next;
+    sfree(toBeDeleted);
+    }
+  }
+  add_to_caravan(animal,0);
+
 }
 
 int get_caravan_load(Caravan caravan)
 {
-  return 0;
+  int load=0;
+
+  Node* curr_node=caravan->head;
+  while(curr_node!=0){
+    load+=get_load((PackAnimal)curr_node->animal);
+    curr_node=curr_node->next;
+  }
+  return load;
 }
 
 void unload(Caravan caravan)
 {
   Node* curr_node = caravan->head;
   while(curr_node != 0){
-    unload(curr_node->pack_animal);
+    unload(curr_node->animal);
     curr_node = curr_node->next;
   }
 }
 
 int get_caravan_speed(Caravan caravan)
 {
-  return 0;
+  if(caravan->head!=0){
+  Node* curr_node=caravan->head;
+  int min_speed=get_actual_speed((PackAnimal)curr_node->animal);
+  while(curr_node!=0){
+    if(min_speed>get_actual_speed((PackAnimal)curr_node->animal))
+    {
+      min_speed=get_actual_speed((PackAnimal)curr_node->animal);
+    }
+    curr_node=curr_node->next;
 }
-
-void optimize_load(Caravan caravan)
-{
-
+return min_speed;
+}
+return 0;
 }
